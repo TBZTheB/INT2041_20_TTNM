699cb7a0275547dd045bd1615aed72031bdd1e08
Bach Quang <dqbach193@gmail.com>
2024-05-16 19:04:38 +0700

    add button to route to login and signup, logout

--- text-to-speech-app/server.js
+++ text-to-speech-app/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const multer = require('multer');
 const { SpeechClient } = require('@google-cloud/speech');
 const fs = require('fs');
@@ -9,35 +10,97 @@ const db = require('./database');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.get('/', (req, res) => {
+    if (req.session.userId) {
+      res.sendFile(__dirname + '/public/index.html');
+    } else {
+      res.redirect('/login');
+    }
+});
+
 app.use(express.static('public'));
+
+//LOG for debug
+app.use((req, res, next)=>{
+    console.log(req.path, req.method);
+    console.log(req.body)
+    console.log(req.session.userId)
+    next();
+})
 
 // Cấu hình multer để lưu trữ tệp tin
 const upload = multer({ dest: 'uploads/' });
 
 const speechClient = new SpeechClient();
 
+
+
 // Endpoint cho trang đăng ký
+// app.post('/register', (req, res) => {
+//     const { username, password } = req.body;
+//     db.run("INSERT INTO users (username, password) VALUES (?, ?)", [username, password], function(err) {
+//         if (err) {
+//             return res.status(500).send("Error registering user");
+//         }
+//         res.redirect('/login.html');
+//     });
+// });
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/public/register.html');
+});
+
 app.post('/register', (req, res) => {
-    const { username, password } = req.body;
-    db.run("INSERT INTO users (username, password) VALUES (?, ?)", [username, password], function(err) {
-        if (err) {
-            return res.status(500).send("Error registering user");
-        }
-        res.redirect('/login.html');
-    });
+const { username, password } = req.body;
+db.run('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], function(err) {
+    if (err) throw err;
+    req.session.userId = this.lastID;
+    res.redirect('/');
+});
 });
 
 // Endpoint cho trang đăng nhập
+// app.post('/login', (req, res) => {
+//     const { username, password } = req.body;
+//     db.get("SELECT * FROM users WHERE username = ? AND password = ?", [username, password], (err, row) => {
+//         if (err) {
+//             return res.status(500).send("Error logging in");
+//         }
+//         if (!row) {
+//             return res.status(400).send("Invalid credentials");
+//         }
+//         res.redirect('/index.html');
+//     });
+// });
+
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/public/login.html');
+});
+
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
-    db.get("SELECT * FROM users WHERE username = ? AND password = ?", [username, password], (err, row) => {
-        if (err) {
-            return res.status(500).send("Error logging in");
+    db.get('SELECT * FROM users WHERE username = ?', [username], (err, user) => {
+        if (err) throw err;
+        if (user && user.password === password) {
+            req.session.userId = user.id;
+            res.redirect('/');
+        } else {
+            res.send('Invalid username or password');
         }
-        if (!row) {
-            return res.status(400).send("Invalid credentials");
-        }
-        res.redirect('/index.html');
+    });
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) throw err;
+        res.redirect('/login');
     });
 });
 
